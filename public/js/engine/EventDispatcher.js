b916edae614a55f15521377f58d57140445e1b90
Milko Kosturkov <mkosturkov@gmail.com>
2014-08-31 15:31:52 +0300

    Allow namespace and global event handlers

--- public/js/engine/EventDispatcher.js
+++ public/js/engine/EventDispatcher.js
@@ -1,13 +1,30 @@
 'use strict';
 function EventDispatcher() {
-	this.handlers = {};
+	this.handlers = {'*': {}};
 	this.reverseMap = {};
 };
 
-EventDispatcher.prototype.nextEventId = 0;
+EventDispatcher.prototype.nextHandlerId = 0;
+
+EventDispatcher.prototype.addGlobalHandler = function(handler) {
+	var id = this.nextHandlerId++;
+	this.handlers['*'][id] = handler;
+	this.reverseMap[id] = '*';
+	return id;
+};
+
+EventDispatcher.prototype.addNamespaceHandler = function(eventNamespace, handler) {
+	var id = this.nextHandlerId++;
+	if(this.handlers[eventNamespace] === undefined) {
+		this.handlers[eventNamespace] = {'*' : {}};
+	}
+	this.handlers[eventNamespace]['*'][id] = handler;
+	this.reverseMap[id] = eventNamespace;
+	return id;
+};
 
 EventDispatcher.prototype.addHandler = function(eventNamespace, eventName, handler) {
-	var id = this.nextEventId++;
+	var id = this.nextHandlerId++;
 	if(this.handlers[eventNamespace] === undefined) {
 		this.handlers[eventNamespace] = {};
 	}
@@ -23,16 +40,50 @@ EventDispatcher.prototype.removeHandler = function(id) {
 	if(this.reverseMap[id] === undefined) {
 		return;
 	}
-	delete this.handlers[this.reverseMap[id].eventNamespace][this.reverseMap[id].eventName][id];
+	if(this.reverseMap[id] === '*') {
+		// Global handlers
+		delete this.handlers['*'][id];
+	} else if(typeof this.reverseMap[id] === 'string') {
+		// Namespace handlers
+		delete this.handlers[this.reverseMap[id]]['*'][id];
+	} else {
+		// Event handlers
+		delete this.handlers[this.reverseMap[id].eventNamespace][this.reverseMap[id].eventName][id];
+	}
 	delete this.reverseMap[id];
 };
 
-EventDispatcher.prototype.fireEvent = function(eventNamespace, eventName, eventData) {
-	if(this.handlers[eventNamespace] === undefined || this.handlers[eventNamespace][eventName] === undefined) {
+EventDispatcher.prototype.fireEvent = function(eventNamespace, eventName, eventData, eventTime) {
+	var event = {
+		namespace: eventNamespace,
+		name: eventName,
+		data: eventData
+	};
+	if(eventTime !== undefined) {
+		event.eventTime = eventTime;
+	} else {
+		event.eventTime = (new Date()).getTime();
+	}
+	// Fire global event handlers
+	if(this.handlers['*'] !== undefined) {
+		for(var id in this.handlers['*']) {
+			this.handlers['x'][id](event);
+		}
+	}
+	if(this.handlers[eventNamespace] === undefined) {
 		return;
 	}
-	for(var id in this.handlers[eventNamespace][eventName]) {
-		this.handlers[eventNamespace][eventName][id](eventData);
+	// Fire namespace event handlers
+	if(this.handlers[eventNamespace]['*'] !== undefined) {
+		for(var id in this.handlers[eventNamespace]['*']) {
+			this.handlers[eventNamespace]['*'][id](event);
+		}
+	}
+	// Fire specific event event handlers
+	if(this.handlers[eventNamespace][eventName] !== undefined) {
+		for(var id in this.handlers[eventNamespace][eventName]) {
+			this.handlers[eventNamespace][eventName][id](eventData);
+		}
 	}
 };
 
